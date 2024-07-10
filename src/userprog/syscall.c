bcb71ccaaf9c4237f7d4ee0dba5ea7fa8a984e45
warrenl <warrencrutcher@gmail.com>
2024-07-10 16:44:27 +0200

    removed unused argument array

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -40,27 +40,6 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-
-/* This array defined the number of arguments each syscall expects.
-   For example, if you want to find out the number of arguments for
-   the read system call you shall write:
-
-   int sys_read_arg_count = argc[ SYS_READ ];
-
-   All system calls have a name such as SYS_READ defined as an enum
-   type, see `lib/syscall-nr.h'. Use them instead of numbers.
- */
-const int argc[] = 
-{
-  /* basic calls */
-  0, 1, 1, 1, 2, 1, 1, 1, 3, 3, 2, 1, 1,
-  /* not implemented */
-  2, 1, 1, 1, 2, 1, 1,
-  /* extended, you may need to change the order of these two (plist, sleep) */
-  0, 1
-};
-
-
 static void
 syscall_handler (struct intr_frame *f)
 {
